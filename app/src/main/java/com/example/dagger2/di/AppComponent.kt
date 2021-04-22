2bd1df23891e1e1cbef518282f5480efe602f2e4
Muhammad Nur Yanhaona <nur.yanhaona@konasl.com>
2021-04-22 10:13:58 +0600

    App component

---
+++ app/src/main/java/com/example/dagger2/di/AppComponent.kt
@@ -0,0 +1,22 @@
+package com.example.dagger2.di
+
+import com.example.dagger2.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+@Component(
+    modules = [AndroidSupportInjectionModule::class]
+)
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: BaseApplication): Builder
+
+        fun build(): AppComponent
+    }
+}
