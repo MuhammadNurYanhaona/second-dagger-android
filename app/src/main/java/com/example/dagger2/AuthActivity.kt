37b42fb6cebdd94f709faae8be91b1498ec6b5bf
Muhammad Nur Yanhaona <nur.yanhaona@konasl.com>
2021-04-22 16:28:27 +0600

    multiple dependencies

--- app/src/main/java/com/example/dagger2/AuthActivity.kt
+++ app/src/main/java/com/example/dagger2/AuthActivity.kt
@@ -1,10 +1,14 @@
 package com.example.dagger2
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
 import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.RequestManager
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
@@ -16,11 +20,22 @@ class AuthActivity : DaggerAppCompatActivity() {
     @Inject
     lateinit var context: Context
 
+    @Inject
+    lateinit var logo: Drawable
+
+    @Inject
+    lateinit var requestManager: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
         Log.d(TAG, "onCreate: $message")
         Log.d(TAG, "onCreate: ${context.packageName}")
+        setLogo()
+    }
+
+    fun setLogo() {
+        requestManager.load(logo).into(findViewById<ImageView>(R.id.login_logo))
     }
 
     companion object {
--- app/src/main/java/com/example/dagger2/di/AppModule.kt
+++ app/src/main/java/com/example/dagger2/di/AppModule.kt
@@ -1,7 +1,13 @@
 package com.example.dagger2.di
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
 import com.example.dagger2.BaseApplication
+import com.example.dagger2.R
 import dagger.Module
 import dagger.Provides
 
@@ -13,12 +19,32 @@ abstract class AppModule {
 
         @JvmStatic
         @Provides
-        fun getString(): String { return "Say something!"}
+        fun providesString(): String { return "Say something!"}
 
         @JvmStatic
         @Provides
         fun getAppContext(application: BaseApplication): Context {
             return application.applicationContext
+        }
+
+        @JvmStatic
+        @Provides
+        fun providesRequestOptions(): RequestOptions {
+            return RequestOptions.placeholderOf(R.drawable.white_background)
+                    .error(R.drawable.white_background)
+        }
+
+        @JvmStatic
+        @Provides
+        fun providesGlideInstance(application: BaseApplication,
+                                  requestOptions: RequestOptions): RequestManager {
+            return Glide.with(application).setDefaultRequestOptions(requestOptions);
+        }
+
+        @JvmStatic
+        @Provides
+        fun providesAppDrawable(application: BaseApplication): Drawable {
+            return ContextCompat.getDrawable(application, R.drawable.logo)!!
         }
     }
 }
